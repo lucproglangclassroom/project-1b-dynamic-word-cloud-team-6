c05fbc4521f90c7eb749423e223906b36b61fb73
andrewdo <ado3@luc.edu>
2024-09-30 21:31:43 -0500

    Added empty input test

--- src/test/scala/topwords/WordCloudTest.scala
+++ src/test/scala/topwords/WordCloudTest.scala
@@ -27,4 +27,20 @@ class WordCloudTest extends AnyFunSuite {
     assert(output.freqMap("today") == 1, "Frequency of 'today' should be 1")
   }
 
+  test("Processing handles empty input") {
+    val words = Iterator.empty[String]
+    val output = new TestOutputSink
+    WordCloud.processing(
+      words = words,
+      cloud_size = 3,
+      length_at_least = 1,
+      window_size = 3,
+      every_K = 1,
+      min_frequency = 1,
+      outputSink = output
+    )
+
+    assert(output.freqMap.isEmpty, "Output should be empty for empty input")
+  }
+
 }
