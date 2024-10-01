8bc541e096bfe24ae95b626c2dad0d0afb828e75
Michael Saban <msaban@luc.edu>
2024-10-01 00:01:47 +0000

    Refactored logic to better implement testability

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -33,12 +33,7 @@ object Main:
         System.exit(4)
     }
 
-    // Separate I/O and logic by creating OutputSink
-    trait OutputSink{
-      def doOutput(value: String): Unit
-    }
-
-     // Handle SIGPIPE signal by exiting 
+    // Handle SIGPIPE signal by exiting 
     Signal.handle(new Signal("PIPE"), new SignalHandler {
       override def handle(sig: Signal): Unit = {
         System.err.println("SIGPIPE detected. Terminating.")
@@ -46,49 +41,29 @@ object Main:
       }
     })
 
-    // Create OutputSink instance that prints the output of fullQueue. This separates I/O from logic
-    object myOutputSink extends OutputSink {
-      def doOutput(value: String) = {
-        try {
-          println(value)
-        } catch {
-          case _: java.io.IOException =>
-            System.err.println("Broken pipe error. Exiting")
-            System.exit(0)
-        }
-      }
-    }
-
-    // Function to process full queue
-    def fullQueue(queue: CircularFifoQueue[String], output:OutputSink): Unit = {
-
-      // Create a variable 'frequency', which is a mutable map of a string and integer
-      val frequency = mutable.Map[String, Int]() 
-
-      // For each word in the current queue: if the string is not in 'frequency', set the word frequency to 0. Add 1 to the frequency.
-      queue.forEach {word => 
-        frequency(word) = frequency.getOrElse(word,0) + 1
-      }
-
-      // Sort by descending frequency and take the first c pairs
-      val sortedfrequency: Seq[(String, Int)] = frequency.toSeq.sortBy(-_._2).filter{case (_,count) => count >=min_frequency}.take(cloud_size)
-
-
-      val out = sortedfrequency.map {case (word,count) => s"$word: $count" }.mkString(" ")
-      output.doOutput(out)
-
-    }
-
-    
     // Set up input Scanner
     val lines = scala.io.Source.stdin.getLines
     val words = 
-      lines.flatMap(l => l.split("(?U)[^\\p{Alpha}0-9']+")).map(_.toLowerCase).filter(_.length >= length_at_least) //.map(_.toLowerCase) satisfies EC for case-insensitivity
+      lines.flatMap(l => l.split("(?U)[^\\p{Alpha}0-9']+")).map(_.toLowerCase) //.map(_.toLowerCase) satisfies EC for case-insensitivity
+
+    // Call WordCloud with given words and arguements
+    WordCloud.processing(words=words, cloud_size=cloud_size, length_at_least=length_at_least, window_size=window_size, every_K=every_K, min_frequency=min_frequency, outputSink=WordCloud.myOutputSink)
+
+    val logger = org.log4s.getLogger
+    logger.debug(f"Cloud Size = $cloud_size Length At Leasts = $length_at_least Window Size = $window_size Every K = $every_K Min Frequency = $min_frequency")
+
+    }
+
+end Main
+
+
+object WordCloud {
+
+  def processing(words: Iterator[String], cloud_size: Int, length_at_least: Int, window_size: Int, every_K: Int, min_frequency: Int, outputSink: OutputSink): Unit = {
 
     val queue = new CircularFifoQueue[String](window_size)
-
     var steps = 0 // Initialize to count steps
-    words.foreach {word =>
+    words.filter(_.length >= length_at_least).foreach {word =>
 
       // Add the word to the queue
       queue.add(word)
@@ -97,16 +72,47 @@ object Main:
       // If the queue is full after adding the word AND steps >= k, call the fullQueue function on the queue. Additionally, reset steps.
       if ((queue.isAtFullCapacity) & (steps >= every_K))  {
         steps = 0
-        fullQueue(queue,myOutputSink)
+        fullQueue(queue,cloud_size, min_frequency, myOutputSink)
       }
     }
+  }
 
-    val logger = org.log4s.getLogger
-    logger.debug(f"Cloud Size = $cloud_size Length At Leasts = $length_at_least Window Size = $window_size Every K = $every_K Min Frequency = $min_frequency")
+  // Separate I/O and logic by creating OutputSink
+  trait OutputSink{
+    def doOutput(value: String): Unit
+  }
 
+  // Create OutputSink instance that prints the output of fullQueue. This separates I/O from logic
+  object myOutputSink extends OutputSink {
+    def doOutput(value: String) = {
+      try {
+        println(value)
+      } catch {
+        case _: java.io.IOException =>
+          System.err.println("Broken pipe error. Exiting")
+          System.exit(0)
+      }
+    }
+  }
 
+  // Function to process full queue
+  def fullQueue(queue: CircularFifoQueue[String], cloud_size: Int, min_frequency: Int, output:OutputSink): Unit = {
 
-  
+    // Create a variable 'frequency', which is a mutable map of a string and integer
+    val frequency = mutable.Map[String, Int]() 
+
+    // For each word in the current queue: if the string is not in 'frequency', set the word frequency to 0. Add 1 to the frequency.
+    queue.forEach {word => 
+      frequency(word) = frequency.getOrElse(word,0) + 1
     }
 
-end Main
+    // Sort by descending frequency and take the first c pairs
+    val sortedfrequency: Seq[(String, Int)] = frequency.toSeq.sortBy(-_._2).filter{case (_,count) => count >=min_frequency}.take(cloud_size)
+
+
+    val out = sortedfrequency.map {case (word,count) => s"$word: $count" }.mkString(" ")
+    output.doOutput(out)
+
+  }
+
+}
